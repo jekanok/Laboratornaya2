6e368a59ea167362d53f785526eaa06172b13841
Vlad.Kliutko <klyutko@icloud.com>
2018-04-27 02:11:06 +0300

    Implemented menu.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ListManager <Double> list = new ListManager<>();
+
+        Scanner in = new Scanner(System.in);
+        String choise = "";
+
+        while (!(choise.equals("y")))
+        {
+            System.out.println();
+
+            System.out.println("- MENU -");
+            System.out.println("1: Add new");
+            System.out.println("2: Delete last");
+            System.out.println("3: Sort");
+            System.out.println("4: Print list");
+            System.out.println("\n5: Exit.\n");
+
+            System.out.print("Enter your variant: ");
+            choise = in.nextLine().toLowerCase();
+
+            switch (choise) {
+                case "1" : {
+                    System.out.print("Enter new element: ");
+                    String line = in.nextLine();
+                    list.addElement(Double.parseDouble(line));
+
+                    break;
+                }
+                case "2" : {
+                    list.removeLast();
+                    break;
+                }
+                case "3" : {
+                    list.sorted();
+                    break;
+                }
+                case "4" : {
+                    list.printList();
+                    break;
+                }
+                case "5" : {
+                    choise = "y";
+
+                    break;
+                }
+
+                default: break;
+            }
+        }
     }
 }
