6662fcffe3b1ba8ab44855f28f93b360ac00adca
Vlad.Kliutko <klyutko@icloud.com>
2018-04-27 16:44:20 +0300

    Update feature.

--- src/com/company/ListManager.java
+++ src/com/company/ListManager.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.stream.Collectors;
 public class ListManager<T extends Number & Comparable> {
 
     List list;
-    boolean isHaveInteger = false;
+
+    boolean isHaveDouble = false;
+    private int countOfDouble = 0;
 
     public ListManager(T initialElement) {
         list = new ArrayList<>();
@@ -18,7 +21,8 @@ public class ListManager<T extends Number & Comparable> {
     public <N extends Number & Comparable> void addElement(N element) {
 
         if (element.getClass() == Double.class) {
-            isHaveInteger = true;
+            isHaveDouble = true;
+            this.countOfDouble++;
         }
 
         if (list.get(list.size() - 1).getClass() == element.getClass())
@@ -32,29 +36,52 @@ public class ListManager<T extends Number & Comparable> {
     }
 
     public void removeLast() {
-        //TODO: make func convertToInteger if last element Double and list don't have any Double element
-        if (!this.list.isEmpty()) this.list.remove(this.list.size() - 1);
+        if (!this.list.isEmpty()) {
+
+            if (this.list.get(this.list.size() - 1) instanceof Double) {
+                this.countOfDouble--;
+            }
+
+            this.list.remove(this.list.size() - 1);
+            if (this.countOfDouble == 0) { this.convertToInteger(); }
+        }
     }
 
-    public void sort() {
-        if (isHaveInteger) { this.convertToDouble(); }
-        this.list = (List<T>) list.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-    }
-
-    public void printList() {
-        for (Object aList : list) System.out.println(aList);
-    }
+    //MARK: Converters
 
     private void convertToDouble() {
 
         List<Double> newList = new ArrayList<>();
 
         for (Object alist : list) {
-            if (alist.getClass() == Double.class) { newList.add((Double)alist); }
+            if (alist instanceof Double) { newList.add((Double)alist); }
             else { newList.add((new Double((Integer)alist))); }
         }
 
         this.list = newList;
+    }
+
+    private void convertToInteger() {
+
+        List<Integer> newList = new ArrayList<>();
+
+        for (Object aList : list) {
+            if (aList instanceof Integer) { newList.add(((Integer) aList)); }
+            else { newList.add(((Double)aList).intValue()); }
+        }
+
+        this.list = newList;
+    }
+
+    //MARK: Other methods
+
+    public void sort() {
+        if (this.isHaveDouble) { this.convertToDouble(); }
+        this.list = (List<T>) list.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+    }
+
+    public void printList() {
+        for (Object aList : list) System.out.println(aList);
     }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,63 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         ListManager listManager = new ListManager(8);
-        listManager.addElement(5);
+        listManager.addElement(9.1);
         listManager.addElement(6);
-        listManager.addElement(-6.3);
+        listManager.addElement(-6);
         listManager.addElement(-2);
 
         listManager.printList();
+
         System.out.println();
         listManager.sort();
         listManager.printList();
 
-//        Scanner in = new Scanner(System.in);
-//        String choise = "";
-//
-//        while (!(choise.equals("y")))
-//        {
-//            System.out.println();
-//
-//            System.out.println("- MENU -");
-//            System.out.println("1: Add new");
-//            System.out.println("2: Delete last");
-//            System.out.println("3: Sort");
-//            System.out.println("4: Print list");
-//            System.out.println("\n5: Exit.\n");
-//
-//            System.out.print("Enter your variant: ");
-//            choise = in.nextLine().toLowerCase();
-//
-//            switch (choise) {
-//                case "1" : {
-//                    System.out.print("Enter new element: ");
-//                    String line = in.nextLine();
-//                    list.addElement(Double.parseDouble(line));
-//
-//                    break;
-//                }
-//                case "2" : {
-//                    list.removeLast();
-//                    break;
-//                }
-//                case "3" : {
-//                    list.sorted();
-//                    break;
-//                }
-//                case "4" : {
-//                    list.printList();
-//                    break;
-//                }
-//                case "5" : {
-//                    choise = "y";
-//
-//                    break;
-//                }
-//
-//                default: break;
-//            }
-//        }
+        System.out.println();
+        listManager.removeLast();
+
+        System.out.println();
+        listManager.printList();
     }
 }
